348f40e6a7b00696d12c0bc69711b9d4f2b30a5f
Jos Menhart <jos.menhart@live.nl>
2024-01-13 22:10:00 +0100

    Removed services

--- JM.RulesEngine.Data/RulesStorageContext.cs
+++ JM.RulesEngine.Data/RulesStorageContext.cs
@@ -1,13 +1,5 @@
 ﻿using JM.RulesEngine.Domain;
-using Microsoft.Azure.Cosmos;
 using Microsoft.EntityFrameworkCore;
-using RulesEngine.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace JM.RulesEngine.Data
 {
@@ -15,19 +7,18 @@ namespace JM.RulesEngine.Data
     {
         public RulesStorageContext(DbContextOptions<RulesStorageContext> options) : base(options)
         {
-            
         }
 
-        public DbSet<WorkflowData> Workflows { get; set; }
+        public DbSet<RulesEngineWorkflow> Workflows { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
             //builder.Entity<T>().
-            
-            builder.Entity<WorkflowData>()
-                    .HasNoDiscriminator()                    
-                    .ToContainer(nameof(WorkflowData))
-                    .HasPartitionKey( x=>x.References)                    
+
+            builder.Entity<RulesEngineWorkflow>()
+                    .HasNoDiscriminator()
+                    .ToContainer(nameof(RulesEngineWorkflow))
+                    .HasPartitionKey(x => x.References)
                     .HasKey(x => x.Id);
         }
     }
---
+++ JM.RulesEngine.Domain/RulesEngineWorkflow.cs
@@ -0,0 +1,37 @@
+﻿using RulesEngine.Actions;
+using RulesEngine.Models;
+
+namespace JM.RulesEngine.Domain
+{
+    public class RulesEngineWorkflow
+    {
+        public RulesEngineWorkflow()
+        {            
+        }
+        public int Id { get; set; }
+        public Guid UId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Workflow Workflow { get; set; }      
+        public int CustomerId { get; set; }
+        
+        public DateTime Created { get; set; }
+        public int CreatedBy { get; set; }
+        public DateTime? Updated { get; set; }
+        public int? UpdatedBy { get; set; }
+        public string CssIcon { get; set; }
+        public string EditModel { get; set; }
+       
+        public bool Enabled { get; set; }
+        public ICollection<string> Properties { get; set; }
+        public string References { get; set; }
+    }
+
+    //public static class RulesEngineActions
+    //{
+    //    public static Dictionary<string, Func<ActionBase>> Actions = new Dictionary<string, Func<ActionBase>>{
+    //                                                                    {"SendEmail", () => new SendEmailAction() },
+    //                                                                    {"CreateAlert", () => new CreateAlertAction() }
+    //                                                                };
+    //}
+}
